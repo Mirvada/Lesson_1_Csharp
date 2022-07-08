24ad32e1307aad02ae93a91a6117144c82b0bbe3
Aleksandra Kolianichenko <mirvada1@yandex.ru>
2022-07-08 14:02:07 +0300

    Created a project about branches

---
+++ project_5/Program.cs
@@ -0,0 +1,25 @@
+﻿//Console.Write("Введите имя пользователя: ");
+//string username = Console.ReadLine();
+
+//if(username == "Маша") //Если написать "маша" то программа выдаст "Привет, маша"
+    //{
+	//Console.WriteLine("Ура, это же Маша");
+    //}
+//else
+    //{
+	//Console.Write("Привет, ");
+	//Console.WriteLine(username);
+    //}
+
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша") //В этом случае регистр не важен.
+    {
+	Console.WriteLine("Ура, это же МАША!");
+    }
+else
+    {
+	Console.Write("Привет, ");
+	Console.WriteLine(username);
+    }
