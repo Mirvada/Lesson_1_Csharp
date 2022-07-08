921b4cb816b9ba59ea015e1823cb914258935e0a
Aleksandra Kolianichenko <mirvada1@yandex.ru>
2022-07-08 12:53:32 +0300

    Created a number division project

---
+++ project_4/Program.cs
@@ -0,0 +1,7 @@
+﻿//int numberA = 12;
+//int numberB = 5;
+//Console.WriteLine(numberA / numberB); //Результат будет 2. только целые число
+
+double numberA = 12;
+double numberB = 5;
+Console.WriteLine(numberA / numberB); //Результат 2,4 цифры со значениями после запятой
