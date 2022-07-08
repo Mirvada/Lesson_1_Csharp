53c97e1f12d32d9632316dcaeebea0589dcadd9a
Aleksandra Kolianichenko <mirvada1@yandex.ru>
2022-07-08 13:46:41 +0300

    Added new info about addition

--- project_3/Program.cs
+++ project_3/Program.cs
@@ -1,4 +1,12 @@
-﻿int numberA = 3;
-int numberB = 5;
-int result = numberA + numberB;
-Console.WriteLine(result);
+﻿//int numberA = 3;
+//int numberB = 5;
+//int result = numberA + numberB;
+//Console.WriteLine(result);
+
+//new Random().Next(min, max) выдает целое число. max-1
+
+int numberA = new Random().Next(1, 10); //числа от 1-9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA / numberB);
